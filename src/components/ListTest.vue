2137ae636b1613c0a3321080002bf97a8ecf8646
corpsele <corpsele555@gmail.com>
2020-04-02 11:10:02 +0800

    First Commit

---
+++ src/components/ListTest.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="listT">
+    <el-tooltip class="item" effect="dark" content="Please select rss which you want to scribe" placement="top-start">
+      <h3>Please select rss which you want to scribe</h3>
+    </el-tooltip>
+    <el-select v-model="data1" @change="selectedAction">
+      <el-option v-for="obj1 in options"
+                 :label="obj1.text"
+                 :value="obj1.value"
+                 :key="obj1.value"
+                 >
+
+      </el-option>
+    </el-select>
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <span>You have selected index = {{selectedIndex}}</span>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">Cancel</el-button>
+    <el-button type="primary" @click="dialogVisible = false">OK</el-button>
+  </span>
+    </el-dialog>
+    <el-table>
+      <el-row>1</el-row>
+      <el-row>2</el-row>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ListTest",
+    data() {
+      return {
+        hintDescription: "Please select rss which you want to scribe",
+        data1: 0,
+        options: [
+          {value: 0, text: "Please Select Value"},
+          {value: 1, text: "Seal Team"}
+        ],
+        dialogVisible: false,
+        selectedIndex: 0
+      }
+    },
+    methods: {
+      selectedAction(num) {
+        console.log("selected value = "+num);
+        this.selectedIndex = num;
+        this.dialogVisible = true;
+      },
+      init() {
+
+      },
+      handleClose(done) {
+        this.$confirm('Really？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+// import Cube from 'cube-ui'
+
+// Vue.use(Cube)
+import VueUi from '@vue/ui'
+import { Editor, EditorContent } from 'tiptap'
+
+Vue.use(VueUi)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
